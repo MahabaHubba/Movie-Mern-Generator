6f16a280cc1ea163bda810a5fbac5f3b01d88ba8
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-09 15:40:16 +1000

    Update server.js
    altered the app.use to inclue the auith middleware and aso imported it form a different file

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,6 +4,7 @@ const db = require('./config/connection');
 const {expressMiddleware} = require('@apollo/server/express4');
 const {ApolloServer} = require('@apollo/server');
 const {typeDefs, resolvers} = require('./schemas')
+const {authMiddleware} = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,7 +19,9 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());  
 
-  app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
   // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -37,6 +40,7 @@ db.once('open', () => {
 });
 }
 
+startApolloServer();
 
 
 
