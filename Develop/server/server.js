859c38f04576365a35b0dfef2cc1198f977dbc2e
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-09 15:36:21 +1000

    Update server.js
    Added POrt for both server and client

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const {expressMiddleware} = require('@apollo/server/express4');
 const {ApolloServer} = require('@apollo/server');
 const {typeDefs, resolvers} = require('./schemas')
@@ -20,15 +19,24 @@ const startApolloServer = async () => {
   app.use(express.json());  
 
   app.use('/graphql', expressMiddleware(server));
-}
 
-// if we're in production, serve client/build as static assets
+  // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
-
-app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
 });
+}
+
+
+
+
