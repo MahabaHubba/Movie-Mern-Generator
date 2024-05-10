67d2b9df98dc1715b67e93bc15944e767aead9cf
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-10 11:38:21 +1000

    Update resolvers.js
    getting all users from the me query

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,3 +1,17 @@
 const {User} = require('../models/User');
 const {signToken, AuthenticationError} = require('../utils/auth');
 
+const resolvers = {
+    Query: {
+        //get all users
+        me: async(parent, args, context) => {
+            console.log('start');
+            if(context.user) {
+                return User.findOne({_id: context.user._id})
+            }
+            throw new Error('Failed to get User')
+        }
+    },
+
+    Mutation
+}
