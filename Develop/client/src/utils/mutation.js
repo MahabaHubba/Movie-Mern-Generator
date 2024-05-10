498829d017caf0db6e3e0d27e6b8644461ef9148
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-10 21:54:17 +1000

    Update mutation.js
    Added Add_user as per readMe, using the addUSer mutation made previosuly

--- Develop/client/src/utils/mutation.js
+++ Develop/client/src/utils/mutation.js
@@ -10,4 +10,16 @@ export const LOGIN_USER = gql`
             }
         }
     }
-`
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email:String!, $password: String!) {
+        adduser(username:$username, email:$email, password:$password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
