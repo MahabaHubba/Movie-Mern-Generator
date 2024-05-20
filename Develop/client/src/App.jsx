c4d393186e88c150c6508943ffc37f840e2674c3
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-20 14:08:13 +1000

    App.jsx
    installation of apoollo client which now works with graphql (tested) no issues anymore

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -13,12 +13,30 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Set up the client to execute authLink middleware
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
