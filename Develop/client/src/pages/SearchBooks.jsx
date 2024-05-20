dfdf80d1cbf75c9668e470d89599baf20d8c46ab
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-21 01:01:37 +1000

    Update SearchBooks.jsx
    Had accidently deleted the search google books api that led to being able to searcg for all the books. Now fixed

--- Develop/client/src/pages/SearchBooks.jsx
+++ Develop/client/src/pages/SearchBooks.jsx
@@ -11,6 +11,7 @@ import Auth from '../utils/auth';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from "@apollo/client";
 import { SAVE_BOOK } from "../utils/mutation";
+import { searchGoogleBooks } from "../utils/API";
 
 
 const SearchBooks = () => {
