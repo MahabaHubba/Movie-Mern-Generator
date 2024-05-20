329f1e9884969023278e06a8709dd0d453313a16
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-20 23:16:05 +1000

    Update SignupForm.jsx
    added installations as per the readme

--- Develop/client/src/components/SignupForm.jsx
+++ Develop/client/src/components/SignupForm.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutation";
 
 const SignupForm = () => {
   // set initial form state
