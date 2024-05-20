44ebc7946069a207af385903665d41183af5fd0a
MahabaHubba <150589335+MahabaHubba@users.noreply.github.com>
2024-05-20 14:04:51 +1000

    Update App.jsx
    imported installation that is required for app.jsx

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 function App() {
   return (
